87b8f2704a4c59ad6e423d4e3e76140a95c1a101
hand1ov1eternity <absurdism@duck.com>
2024-10-21 12:48:46 +0200

    rectify GET route for the homepage

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //GET route for the homepage
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+  res.send('Welcome to My Movie API!');
 });
 
 // GET route for the "/movies" endpoint
