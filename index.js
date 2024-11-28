d7f3b05ba1aaede4ab2c10b5279ad0fcb7c0a004
hand1ov1eternity <absurdism@duck.com>
2024-11-28 14:14:37 +0100

    syntax change

--- index.js
+++ index.js
@@ -113,10 +113,10 @@ app.get('/users/:username', passport.authenticate('jwt', { session: false }), as
 
 // Register new user
 app.post('/users', [
-  check('Username', 'Username is required').isLength({min: 5}),
-  check('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
-  check('Password', 'Password is required').not().isEmpty(),
-  check('Email', 'Email does not appear to be valid').isEmail()
+  check('username', 'Username is required').isLength({min: 5}),
+  check('username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric(),
+  check('password', 'Password is required').not().isEmpty(),
+  check('email', 'Email does not appear to be valid').isEmail()
 ], async (req, res) => {
 
   let errors = validationResult(req);
