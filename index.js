6b9e5b2a57b619b152d40d7d171d1de8ebb7ce69
hand1ov1eternity <absurdism@duck.com>
2024-11-28 09:51:16 +0100

    allow certain site to be given access to my domain

--- index.js
+++ index.js
@@ -10,7 +10,18 @@ const Users = Models.User;
 const cors = require('cors');
 
 // Run CORS
-app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost:27017/cfDB', {
