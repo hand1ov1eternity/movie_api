5ee9087c8b4660d37db6223b9b50d12590680a44
hand1ov1eternity <absurdism@duck.com>
2024-12-02 11:48:33 +0100

    test mongo connect

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ app.use(cors({
 }));
 
 /*// Connect to MongoDB
-mongoose.connect('mongodb+srv://revolutionarygr:Tax1diaaxNAI@myflixdb.piv4e.mongodb.net/', {
+mongoose.connect('https://movie-api-bqfe.onrender.com', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });*/
 
 // Connect to MongoDB
-mongoose.connect( 'mongodb+srv://revolutionarygr:Tax1diaaxNAI@myflixdb.piv4e.mongodb.net/', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect( 'https://movie-api-bqfe.onrender.com', { useNewUrlParser: true, useUnifiedTopology: true });
 
 // Middleware to log all requests and parse JSON
 app.use(morgan('common'));
