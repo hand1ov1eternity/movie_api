142a48b241a9a1fa3eaf998575aedbf87a4bc23f
hand1ov1eternity <absurdism@duck.com>
2024-12-13 12:45:05 +0100

    reinstall passport authenticate

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ app.get('/', (req, res) => {
 });
 
 // Return all movies
-app.get('/movies', async (req, res) => {
+app.get('/movies', passport.authenticate('jwt', { session: false }), async (req, res) => {
   try {
     const movies = await Movies.find(); // Fetch all movies from MongoDB
     if (!movies.length) {
